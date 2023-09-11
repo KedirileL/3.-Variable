9aa6eb2757ab515486bca8abf7a82b34e1b5cee9
user <user@LAPTOP-KAN0NU4I>
2023-09-11 10:46:18 +0200

    Add project files.

---
+++ 3. Variable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaration
+            string name;
+            int age;
+            name = "Andani";
+            age= 10;
+
+            //printing to consule using + for concatenation
+
+            Console.WriteLine("Hi " + name);
+            Console.WriteLine("You are " + age);
+
+            //Change the values of the variables
+
+            name = "Lekgale";
+            age = 19;
+
+            Console.WriteLine("He never like his name, at age " + age + " he changed to " + name);
+        }
+    }
+}
